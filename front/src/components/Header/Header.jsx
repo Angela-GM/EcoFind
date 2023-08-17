438b6707b9ba2ecf3a5e47f6a31678ff5ce485e6
Angela-GM <angelagm0112@gmail.com>
2023-08-17 13:33:03 +0200

    update

--- front/src/components/Header/Header.jsx
+++ front/src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { IoCartOutline } from 'react-icons/io5';
 import './Header.css';
 import NotificationBadge from '../NotificationBadge/NotificationBadge';
 import { useLikesContext } from '../NotificationBadge/LikesContext';
 import ModalComponent from '../Modal/ModalComponent';
 import logo from '../../assets/images/logo-icon.png';
+import { Link } from 'react-router-dom';
 
 
 
@@ -25,7 +26,9 @@ const Header = () => {
     <div className='header'>
       <div className='logo'>
         {/* Aquí puedes colocar la imagen de tu logo */}
+        <Link to={"/"} >
         <img src={logo} alt="Logo" />
+        </Link>
       </div>
       <div className='user-login'>
         {/* Aquí colocamos el botón de inicio de sesión con el ícono de usuario */}
@@ -36,7 +39,7 @@ const Header = () => {
         <ModalComponent isOpen={modalIsOpen} onClose={handleCloseModal} />
     
         {/* Icono de carrito de compras */}
-        <IoCartOutline className='icon' size={30} data-testid="cart-icon"/>
+        <IoCartOutline className='icon' size={30} />
         {/* Icono de lupa de buscador */}
         
       </div>
