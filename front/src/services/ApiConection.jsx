65abece5d4925b37a9a0c6e41953a55b1bc93918
Angela-GM <angelagm0112@gmail.com>
2023-08-17 14:48:39 +0200

    arrreglar ruta imagenes

--- front/src/services/ApiConection.jsx
+++ front/src/services/ApiConection.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiService = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'http://localhost:8000' || 'https://xzk6zy-8000.csb.app',
 });
 
 export const fetchProducts = async (searchQuery) => { // Agrega el parámetro searchQuery
